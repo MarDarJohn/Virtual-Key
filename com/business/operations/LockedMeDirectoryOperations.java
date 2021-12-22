841276a45f67efbc5047577ac8cb2d974e8d9749
Marcus Johnson <infotech.mdj@gmail.com>
2021-12-22 11:59:29 -0800

    Virtual Key
    First Commit

---
+++ com/business/operations/LockedMeDirectoryOperations.java
@@ -0,0 +1,19 @@
+package com.business.operations;
+
+import com.exceptions.DirectoryNotFoundException;
+import com.exceptions.FailCreateDirectoryException;
+import com.exceptions.FileDoesNotExistException;
+import com.exceptions.IsNotDirectoryException;
+import com.exceptions.NullDirectoryException;
+
+@SuppressWarnings("unused")
+public interface LockedMeDirectoryOperations {
+	
+	public void displayDirectory(String directoryName) throws NullDirectoryException;
+	
+	public void makeDirectory(String directoryName) throws FailCreateDirectoryException;
+	
+	public void validDirectory(String directoryName) throws IsNotDirectoryException, DirectoryNotFoundException;
+	
+	
+}
